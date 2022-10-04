e4cc1083c3150679322e4636acc80727e73ff311
GlaurungSH <chiachenkosergey@gmail.com>
2022-10-05 00:27:55 +0300

    Movement of the gun across the field

--- space_invaders/controls.py
+++ space_invaders/controls.py
@@ -7,14 +7,14 @@ def events(gun):
     for event in pygame.event.get():  # Collecting all user events
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:  # right key
-            if event.key == pygame.K_d:
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:  # right key
                 gun.mright = True
-            elif event.key == pygame.K_a:
+            elif event.key == pygame.K_a:  # left key
                 gun.mleft = True
-        elif event.type == pygame.KEYUP:  # right key
-            if event.key == pygame.K_d:
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:  # right key
                 gun.mright = False
-            elif event.key == pygame.K_a:
+            elif event.key == pygame.K_a:  # left key
                 gun.mleft = False
 
